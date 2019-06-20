4851375e24d4d0be8e978ffd1e5fc57ece8bab49
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2019-06-21 01:23:01 +0300

    update customize-output example

--- _examples/customize-output/main.go
+++ _examples/customize-output/main.go
@@ -3,17 +3,40 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"strings"
 
 	"github.com/kataras/golog"
 )
+
+// https://golang.org/doc/go1.9#callersframes
+func getCaller() (string, int) {
+	var pcs [10]uintptr
+	n := runtime.Callers(1, pcs[:])
+	frames := runtime.CallersFrames(pcs[:n])
+
+	for {
+		frame, more := frames.Next()
+
+		if !strings.HasSuffix(frame.File, "github.com/kataras/golog") && frame.Func.Name() != "main.getCaller" {
+			return frame.File, frame.Line
+		}
+
+		if !more {
+			break
+		}
+	}
+
+	return "?", 0
+}
 
 func main() {
 
 	golog.Handle(func(l *golog.Log) bool {
 		prefix := golog.GetTextForLevel(l.Level, true)
-		pc, fn, line, _ := runtime.Caller(7)
-		message := fmt.Sprintf("%s line %d (%s) (%s) %s: %s",
-			prefix, line, runtime.FuncForPC(pc).Name(), fn, l.FormatTime(), l.Message)
+
+		filename, line := getCaller()
+		message := fmt.Sprintf("%s %s [%s:%d] %s",
+			prefix, l.FormatTime(), filename, line, l.Message)
 
 		if l.NewLine {
 			message += "\n"
